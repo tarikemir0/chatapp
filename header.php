0627e3cb5a8e969ee9c4e96d2484e2371c12a0d4
KaanTuran28 <kaanturan626@gmail.com>
2024-01-21 00:32:14 +0300

    Ayarlar Düzeltme

--- header.php
+++ header.php
@@ -7,5 +7,5 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Realtime Chat App | CodingNepal</title>
   <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
